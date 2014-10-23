fe53b5319854793f48bf7e79d689861d654c14d4
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2014-10-23 22:01:23 +0200

    chore: fix release task

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function(grunt) {
         updateConfigs: [],
         commit: true,
         commitMessage: 'Release v%VERSION%',
-        commitFiles: ['package.json', 'bower.json', 'dist'],
+        commitFiles: ['package.json', 'bower.json'],
         createTag: true,
         tagName: 'v%VERSION%',
         tagMessage: 'Version %VERSION%',
